826be33254c7790cf280d86144c4047eeaa27784
Jaurès RATSIMBAZAFIHARIVOLA <74584503+matrix11061991@users.noreply.github.com>
2023-01-02 16:11:16 +0200

    Create FormGenerator.js

---
+++ Mini/FormGenerator.js
@@ -0,0 +1,29 @@
+function generateFormHTML(fields) {
+  // Initialise le code HTML du formulaire
+  let formHTML = '<form>';
+  // Itère sur chaque champ de formulaire
+  for (const field of fields) {
+    // Génère le code HTML du champ de formulaire
+    let fieldHTML = '';
+    if (field.type === 'text') {
+      fieldHTML = `<label>${field.label}: <input type="text" name="${field.name}"></label>`;
+    } else if (field.type === 'checkbox') {
+      fieldHTML = `<label><input type="checkbox" name="${field.name}"> ${field.label}</label>`;
+    }
+    // Ajoute le code HTML du champ de formulaire au code HTML du formulaire
+    formHTML += fieldHTML;
+  }
+  // Ferme la balise form et renvoie le code HTML complet du formulaire
+  formHTML += '</form>';
+  return formHTML;
+}
+// Crée un tableau de champs de formulaire
+const fields = [
+  { type: 'text', label: 'Nom', name: 'name' },
+  { type: 'text', label: 'Adresse e-mail', name: 'email' },
+  { type: 'checkbox', label: 'Recevoir des newsletters', name: 'newsletter' },
+];
+// Génère le code HTML du formulaire
+const formHTML = generateFormHTML(fields);
+// Ajoute le formulaire à la page web
+document.getElementById('form-container').innerHTML = formHTML;
